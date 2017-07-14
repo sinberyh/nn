61b16a3646db041d160c687fdc35b2f3dee80c81
sinberyh <592582598@qq.com>
2017-07-14 16:02:22 +0800

    add for range(10)

---
+++ nn/main.py
@@ -0,0 +1,17 @@
+#%%
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+from sklearn.datasets import load_iris
+
+iris = load_iris()
+
+x = np.linspace(0, 20, 100)
+plt.plot(x, np.sin(x))
+plt.show()
+print(iris.data.shape, iris.target.shape)
+iris.data = iris.data.reshape((150, 2, 2))
+print(iris.data.shape)
+
+for i in range(10):
+    print(i)
